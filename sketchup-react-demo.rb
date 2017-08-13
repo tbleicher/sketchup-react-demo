2bd9251f6063de2c1c3a4015539d76ad9e2bbbd4
Thomas Bleicher <tbleicher@gmail.com>
2017-08-13 13:13:19 -0500

    adding author and copyright to extension info

--- sketchup-react-demo.rb
+++ sketchup-react-demo.rb
@@ -2,6 +2,8 @@ require 'sketchup.rb'
 require 'extensions.rb'
 
 reactdemo = SketchupExtension.new('SketchUp React Demo', 'sketchup-react-demo/main.rb')
-reactdemo.version = '1.0'
+reactdemo.author = 'tbleicher@gmail.com'
+reactdemo.copyright = 'Thomas Bleicher 2017'
+reactdemo.version = '0.1.1'
 reactdemo.description = 'Example of HtmlDialog with React.js.'
 Sketchup.register_extension(reactdemo, true)
